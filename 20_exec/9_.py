030d226c1b84fe2f44252878f1aaa5fdb6b36bc0
ScriptErrorVGM <mixajlov.grigory@gmail.com>
2021-12-13 10:31:59 +1000

    ver.2

--- 20_exec/9_.py
+++ 20_exec/9_.py
@@ -1,16 +1,20 @@
 def main():
     def isPrime(n):
-        if n == 1:
+        if not n < 0 :
+            if n == 1:
+                return False
+            if n % 2 == 0:
+                return n == 2 and n > 0
+            d = 3
+            while d * d <= n and n % d != 0:
+                d += 2
+            return d * d > n
+        else:
             return False
-        if n % 2 == 0:
-            return n == 2
-        d = 3
-        while d * d <= n and n % d != 0:
-            d += 2
-        return d * d > n
 
-    print(isPrime(2))
-    print(isPrime(6))
+    print(isPrime(-1))
+    print(isPrime(1))
+    print(isPrime(11))
 
 if __name__ == "__main__":
-    main()    
+    main()  
